ab56114201acc337360402287d750400cd3c8d2f
Tyler <ty.sheaffer@gmail.com>
2022-10-04 16:13:15 -1000

    initial commit

---
+++ printer.js
@@ -0,0 +1,11 @@
+const array = [];
+
+const timeStarted = new Date();
+for (let i = 0; i < 100000000; i++) {
+    array.push(i);
+    if (i % 1000000 === 0) {
+        console.log('OUTPUT: On iteration ', i, 'after', `${new Date() - timeStarted}ms`);
+    }
+}
+
+console.log('OUTPUT: Finished with all', array.length, 'iterations');
