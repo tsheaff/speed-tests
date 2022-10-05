2d81132b00f970c0fe8dcf65539cc5ed94c72b3a
Tyler <ty.sheaffer@gmail.com>
2022-10-04 16:18:12 -1000

    don't push

--- printer.js
+++ printer.js
@@ -1,8 +1,8 @@
 const array = [];
 
-const timeStarted = new Date();
+// const timeStarted = new Date();
 for (let i = 0; i < 1000000000; i++) {
-    array.push(i);
+    // array.push(i);
     if (i % 100000000 === 0) {
         console.log('OUTPUT: On iteration ', i, 'after', `${new Date() - timeStarted}ms`);
     }
